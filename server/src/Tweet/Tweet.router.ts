889f4df828e63cdc39d0a74dde42a1ca657b3372
tamar3252 <tamarcohen32@gmail.com>
2024-01-29 08:44:03 +0200

    a

--- server/src/Tweet/Tweet.router.ts
+++ server/src/Tweet/Tweet.router.ts
@@ -6,7 +6,7 @@ const {authUser} =require('../Auth')
 const routerTweet = expressTweet.Router();
 
 routerTweet.get("/all_tweets",TweetCtrl.getAllTweets)//also for guest
-routerTweet.get("/tweets_with_follower",authUser,TweetCtrl.getTweetsWithFollower)//לבדוק
+routerTweet.get("/tweets_with_follower",authUser,TweetCtrl.getTweetsWithFollower)
 routerTweet.get("/tweet",TweetCtrl.getTweet)//also for guest
 routerTweet.post("/add_tweet",authUser,TweetCtrl.addTweet)//בדיקה אם הכניס טקסט
 routerTweet.post("/add_comment_to_tweet",authUser,TweetCtrl.addComment)//בדיקה אם הכניס טקסט
