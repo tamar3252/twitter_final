eb5df946788d522c10cea42120bdecaca3cd85a0
tamar3252 <tamarcohen32@gmail.com>
2024-01-29 08:39:53 +0200

    s

--- server/src/Tweet/Tweet.router.ts
+++ server/src/Tweet/Tweet.router.ts
@@ -6,10 +6,11 @@ const {authUser} =require('../Auth')
 const routerTweet = expressTweet.Router();
 
 routerTweet.get("/all_tweets",TweetCtrl.getAllTweets)//also for guest
-routerTweet.get("/tweets_with_follower",authUser,TweetCtrl.getTweetsWithFollower)
+routerTweet.get("/tweets_with_follower",authUser,TweetCtrl.getTweetsWithFollower)//לבדוק
 routerTweet.get("/tweet",TweetCtrl.getTweet)//also for guest
-routerTweet.post("/add_tweet",authUser,TweetCtrl.addTweet)//בדיקה אם הכניס טקסט
-routerTweet.patch("/delete_tweet",authUser,TweetCtrl.deleteTweet)
+routerTweet.post("/add_tweet",authUser,TweetCtrl.addTweet)
+routerTweet.post("/add_comment_to_tweet",authUser,TweetCtrl.addComment)
+routerTweet.delete("/delete_tweet/:tweet_id",authUser,TweetCtrl.deleteTweet)
 
 module.exports = routerTweet;
 export default routerTweet
